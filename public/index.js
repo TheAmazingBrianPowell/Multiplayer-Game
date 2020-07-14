e8aaaa6fc9e0563fafe66f83991b82d184d96f4d
Brian Powell <blobfish@Verelles-MacBook-Air.local>
2020-07-14 16:11:05 -0700

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,208 @@
+var close = document.getElementById("close");
+var userExpand = document.getElementById("userExpand");
+close.addEventListener("click", () => {
+	userExpand.style.display = "none";
+	document.getElementById('signup').style.display = 'none';
+	document.getElementById('login').style.display = 'none';
+});
+
+//setTimeout(() => {
+//	userExpand.style.display = "inline-block";
+//}, 5000);
+
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId());
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail());
+}
+
+let keysPressed = {};
+let mouseIsPressed = false;
+
+
+document.addEventListener('keydown', (event) => {
+	keysPressed[event.key] = true;
+});
+
+document.addEventListener('keyup', (event) => {
+	keysPressed[event.key] = false;
+});
+
+document.addEventListener('mouseup', (event) => {
+	mouseIsPressed = true;
+});
+
+document.addEventListener('mousedown', (event) => {
+	mouseIsPressed = false;
+});
+
+var touch = false;
+var touchX = 0;
+var touchY = 0;
+
+document.body.addEventListener('touchstart', function(e) {
+	touch = true;
+	touchX = e.touches[0].clientX;
+	touchY = e.touches[0].clientY;
+}, false);
+
+document.body.addEventListener('touchend', function(e) {
+	touch = false;
+}, false);
+
+document.body.addEventListener('touchmove', function(e) {
+	touch = true;
+	touchX = e.touches[0].clientX;
+	touchY = e.touches[0].clientY;
+}, false);
+
+var socket;
+var x = 0,y = 0;
+
+document.body.addEventListener("mousemove", function(e) {
+	x = e.clientX;
+	y = e.clientY;
+});
+
+//document.getElementById("play").addEventListener("click", function() {
+//this.style.display = "none";
+socket = io();
+socket = io.connect('https://game8.herokuapp.com');
+var id;
+var elements = [];
+
+//var elem = document.getElementById('game');
+//var two = new Two({fullscreen: true}).appendTo(elem);
+//
+var players = {};
+var flags = [];
+
+const app = new PIXI.Application({
+width:innerWidth,
+height:innerHeight,
+antialias: true,
+});
+document.body.appendChild(app.view);
+window.onresize = function() {
+	app.renderer.resize(innerWidth, innerHeight);
+}
+
+PIXI.Loader.shared
+.load(setup);
+function setup() {
+	//Create the cat sprite
+	//	cat = new PIXI.Sprite(PIXI.Loader.shared.resources["oof.jpg"].texture);
+	//	cat.x = innerWidth*0.5;
+	//	cat.y = innerHeight*0.5;
+	//	cat.anchor.x = 0.5;
+	//	cat.anchor.y = 0.5;
+	//	cat.rotation = 0.5;
+	//Add the cat to the stage
+	//app.stage.addChild(cat);
+	app.ticker.add(gameLoop);
+}
+let flagContainer = new PIXI.Container();
+flagContainer.zIndex = 0;
+let playerContainer = new PIXI.Container();
+playerContainer.zIndex = 1;
+
+socket.on("state", (data) => {
+	if(id) {
+		for(var i in data.players) {
+			if(!players[i]) {
+				players[i] = new PIXI.Graphics();
+				players[i].beginFill(data.players[i].side == "red" ? 0xFF0000 : 0x3377FF);
+				players[i].drawCircle(0, 0, 20);
+				players[i].x = data.players[i].x;
+				players[i].y = data.players[i].y;
+				players[i].endFill();
+				app.stage.addChild(players[i]);
+			}
+			players[i].tX = data.players[i].x;
+			players[i].tY = data.players[i].y;
+			players[i].width = data.players[i].radius*2;
+			players[i].height = data.players[i].radius*2;
+			players[i].side = data.players[i].side;
+			players[i].hasFlag = data.players[i].hasFlag;
+		}
+		if(!flags[0]) {
+			flags[0] = new PIXI.Graphics();
+			flags[0].lineStyle(4, 0xFF0000, 1);
+			flags[0].drawCircle(0,0,22);
+			app.stage.addChild(flags[0]);
+			flags[0].x = data.flags[0].x;
+			flags[0].y = data.flags[0].y;
+			flags[1] = new PIXI.Graphics();
+			flags[1].lineStyle(4, 0x3377FF, 1);
+			flags[1].drawCircle(0,0,22);
+			app.stage.addChild(flags[1]);
+			flags[1].x = data.flags[1].x;
+			flags[1].y = data.flags[1].y;
+		}
+	}
+});
+
+socket.on('connect', () => {
+	if(!id) {
+		socket.emit('join', '', (data) => {
+			id = data;
+		});
+		socket.emit('getElements', '', (data) => {
+			for(var i = 0; i < data.length; i+=4) {
+				elements[i*0.25] = new PIXI.Graphics();
+				elements[i*0.25].beginFill(0xaaaaaa);
+				elements[i*0.25].drawRect(data[i], data[i+1],data[i+2], data[i+3]);
+				elements[i*0.25].endFill();
+				app.stage.addChild(elements[i*0.25]);
+			}
+		});
+	}
+});
+
+function gameLoop() {
+	if(players[id]) {
+		if(keysPressed['ArrowRight'] && keysPressed['ArrowDown']) socket.emit('move', [1,1]);
+		else if(keysPressed['ArrowRight'] && keysPressed['ArrowUp']) socket.emit('move',  [1,-1]);
+		else if(keysPressed['ArrowLeft'] && keysPressed['ArrowDown']) socket.emit('move',  [-1,1]);
+		else if(keysPressed['ArrowLeft'] && keysPressed['ArrowUp']) socket.emit('move',  [-1,-1]);
+		else if(keysPressed['ArrowRight']) socket.emit('move', [1,0]);
+		else if(keysPressed['ArrowLeft']) socket.emit('move', [-1,0]);
+		else if(keysPressed['ArrowDown']) socket.emit('move', [0,1]);
+		else if(keysPressed['ArrowUp']) socket.emit('move', [0,-1]);
+		else if(touch) socket.emit('move', [Math.abs(touchX-innerWidth*0.5) > 20 ? touchX-innerWidth*0.5 : 0, Math.abs(touchY-innerHeight*0.5) > 20 ? touchY-innerHeight*0.5 : 0]);
+		for(var i in players) {
+			players[i].x += (players[i].tX - players[i].x) * 0.16;
+			players[i].y += (players[i].tY - players[i].y) * 0.16;
+			if(players[i].hasFlag) {
+				flags[players[i].side == "red" ? 1:0].x = players[i].x;
+				flags[players[i].side == "red" ? 1:0].y = players[i].y;
+			}
+		}
+		app.stage.setTransform(-players[id].x + innerWidth * 0.5, -players[id].y + innerHeight * 0.5);
+		if(mouseIsPressed) {
+			socket.emit("fired");
+		}
+	}
+}
+
+//collision = two.makeCircle(0,0,5);
+//collision.fill = 'rgba(255,0,0,255)';
+//
+//two.bind('update', function(frameCount) {
+//
+//	for(var i in players) {
+//		if(players[id]) {
+//		if(!players[i].shape) players[i].shape = two.makeCircle(0,0,20);
+//		//if(!players[i].shape2) players[i].shape2 = two.makeCircle(0,0,20);
+//		players[i].x += (playersT[i].x - players[i].x) * 0.16;
+//		players[i].y += (playersT[i].y - players[i].y) * 0.16;
+//		players[i].shape.translation.set(players[i].x-players[id].x + innerWidth*0.5, players[i].y-players[id].y + innerHeight*0.5);
+//		//players[i].shape2.translation.set(playersT[i].x, playersT[i].y);
+//		}
+//	}
+//	if(players[id]) {
+//		elementsG.translation.set(-players[id].x+innerWidth*0.5,-players[id].y+innerHeight*0.5);
+//	}
+//}).play();
---
+++ server2.js
@@ -0,0 +1,136 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', express.static('public'));
+
+app.post('/', (req, res) => {
+	console.log(req.body);
+	res.sendStatus(200);
+});
+
+var gameOver = false;
+const speed = 10;
+const elements = [0,1,10,1009,
+				  1,0,2009,10,
+				  0,1000,2009,10,
+				  2000,0,10,1009,
+				  300,100,100,100,
+				  300,800,100,100,
+				  300,450,100,100,
+				  500,200,10,200,
+				  500,600,10,200,
+				  700,495,250,10,
+				  1600,100,100,100,
+				  1600,800,100,100,
+				  1600,450,100,100,
+				  1490,200,10,200,
+				  1490,600,10,200,
+				  1050,495,250,10,
+				  ];
+
+const gameState = {
+players:{},
+flags:[],
+red:0,
+blue:0,
+numPlayers:0,
+bullets:{}
+}
+
+server.listen(process.env.PORT || 8080);
+
+io.on('connection', socket => {
+	socket.on('join', (data, response) => {
+		if(gameState.numPlayers == 0 || !gameState.flags[0]) {
+			gameState.flags[0] = {x:50, y:500};
+			gameState.flags[1] = {x:1950, y:500};
+		}
+		console.log('Client connected...');
+		response(socket.id);
+		gameState.players[socket.id] = {
+		x: (gameState.red > gameState.blue) ? 1550 : 450,
+		y: 500,
+		radius: 20,
+		lastMoved: new Date(),
+		pX: 0,
+		pY: 0,
+		side:(gameState.red > gameState.blue) ? "blue" : "red",
+		respawnTimer:0,
+		hasFlag:false
+		}
+		if(gameState.red > gameState.blue) {
+			gameState.blue++;
+		} else {
+			gameState.red++;
+		}
+		gameState.numPlayers++;
+	});
+	socket.on('getElements', (data, response) => {
+		response(elements);
+	});
+	socket.on('move', (data) => {
+		try {
+			now = new Date();
+			var direction = getDirection(data);
+			if(now - gameState.players[socket.id].lastMoved >= 17) {
+				gameState.players[socket.id].x += direction[0] * speed;
+				gameState.players[socket.id].y += direction[1] * speed;
+			} else {
+				gameState.players[socket.id].x += direction[0] * speed*(now - gameState.players[socket.id].lastMoved) / 17;
+				gameState.players[socket.id].y += direction[1] * speed*(now - gameState.players[socket.id].lastMoved) / 17;
+			}
+			gameState.players[socket.id].lastMoved = new Date();
+			for(var i = 0; i < elements.length; i+=4) {
+				if(Math.abs(elements[i]+elements[i+2]*0.5-gameState.players[socket.id].x) < elements[i+2] * 0.5 + gameState.players[socket.id].radius && Math.abs(elements[i+1]+elements[i+3]*0.5-gameState.players[socket.id].y) < elements[i+3] * 0.5 + gameState.players[socket.id].radius) {
+					var lineValueX = lineCollision(gameState.players[socket.id].x, gameState.players[socket.id].y, gameState.players[socket.id].pX, gameState.players[socket.id].pY, elements[i] - gameState.players[socket.id].radius, elements[i+1] - gameState.players[socket.id].radius, elements[i] - gameState.players[socket.id].radius, elements[i+1] + elements[i+3] + gameState.players[socket.id].radius) || lineCollision(gameState.players[socket.id].x, gameState.players[socket.id].y, gameState.players[socket.id].pX, gameState.players[socket.id].pY, elements[i] + elements[i+2] + gameState.players[socket.id].radius, elements[i+1] - gameState.players[socket.id].radius, elements[i] + elements[i+2] + gameState.players[socket.id].radius, elements[i+1] + elements[i+3] + gameState.players[socket.id].radius);
+					var lineValueY = lineCollision(gameState.players[socket.id].x, gameState.players[socket.id].y, gameState.players[socket.id].pX, gameState.players[socket.id].pY, elements[i] - gameState.players[socket.id].radius, elements[i+1] - gameState.players[socket.id].radius, elements[i] + elements[i+2] + gameState.players[socket.id].radius, elements[i+1] - gameState.players[socket.id].radius) || lineCollision(gameState.players[socket.id].x, gameState.players[socket.id].y,gameState.players[socket.id].pX, gameState.players[socket.id].pY, elements[i] - gameState.players[socket.id].radius, elements[i+1] + elements[i+3] + gameState.players[socket.id].radius, elements[i] + elements[i+2] + gameState.players[socket.id].radius, elements[i+1] + elements[i+3] + gameState.players[socket.id].radius);
+					if(lineValueX) {
+						gameState.players[socket.id].x = lineValueX[0];
+					}
+					if(lineValueY) {
+						gameState.players[socket.id].y = lineValueY[1];
+					}
+				}
+			}
+			if(Math.hypot(gameState.players[socket.id].x - gameState.flags[gameState.players[socket.id].side == "blue" ? 0:1].x, gameState.players[socket.id].y - gameState.flags[gameState.players[socket.id].side == "blue" ? 0:1].y) < 20) {
+				gameState.players[socket.id].hasFlag = true;
+				gameState.players[socket.id].radius = 24;
+			}
+			gameState.players[socket.id].pX = gameState.players[socket.id].x;
+			gameState.players[socket.id].pY = gameState.players[socket.id].y;
+		} catch(e) {
+			console.log("Uh oh: " + e.stack);
+		}
+	});
+	
+	socket.on("fired", () => {
+		
+	});
+	
+	socket.on('disconnect', () => {
+		if(gameState.numPlayers == 0) {
+			gameState.players = {};
+		}
+	});
+});
+
+setInterval(() => {
+	io.sockets.emit('state', gameState);
+}, 50);
+
+const getDirection = (data) => {
+	var length = Math.sqrt(data[0]*data[0]+data[1]*data[1]);
+	return [data[0]/length || 0, data[1]/length || 0];
+}
+
+const lineCollision = (x1, y1, x2, y2, x3, y3, x4, y4) => {
+	var a = ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+	var b = ((x2-x1)*(y1-y3) - (y2-y1)*(x1-x3)) / ((y4-y3)*(x2-x1) - (x4-x3)*(y2-y1));
+	if (a >= 0 && a <= 1 && b >= 0 && b <= 1) return [x1 + (a * (x2-x1)), y1 + (a * (y2-y1))];
+	return undefined;
+};
